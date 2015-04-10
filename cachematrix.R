693ca4d1ca048746f5308027e08d1202e129fe82
liammcnamara <liamsmcnamara@gmail.com>
2015-04-10 22:45:01 +1000

    Assignment submission
    Necessary changes have been made to cachematrix in order to satisfy the
    assessment task.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is usually a costly computation and there may be some benefit 
+## to caching the inverse of a matrix rather than compute it repeatedly 
+## (there are also alternatives to matrix inversion that we will not discuss here). 
 
-## Write a short comment describing this function
+## makeCacheMatrix
+## This function creates the matrix object as well as 
+## defining get and set functions.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse <- NULL
+  
+  # define set 
+  set <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  }
+  
+  # define get
+  get <- function() x
+  
+  # define setinverse, sets inverse matrix to parameter par.
+  setinverse <- function(par) inverse <<- par
+  
+  # define getinverse, returns inverse.
+  getinverse <- function() inverse
+  
+  # creates list of get and set.
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve
+## This function calculates the inverse but first checks if the function does not 
+## already exist in cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  ## Check if x contains an inverse.
+  inverse <- x$getinverse()
+  
+  ## if inverse does exist the return it.
+  if(!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse)
+  }
+  ## Otherwise if the inverse does not exist then
+  ## get the matrix we want the inverse of.
+  data <- x$get()
+  
+  ## Solve for that inverse.
+  inverse <- solve(data, ...)
+  
+  ## Save that inverse to cache.
+  x$setinverse(inverse)
+  
+  ## Return inverse.
+  inverse
+  
 }
